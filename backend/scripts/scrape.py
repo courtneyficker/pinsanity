afd2af32f92bb842c19109f1e9f9f65008773ff8
Courtney Ficker <courtney.ficker@gmail.com>
2023-03-16 13:14:25 -0700

    Fixed scrape script, populated master pin json list

--- backend/scripts/scrape.py
+++ backend/scripts/scrape.py
@@ -1,48 +1,45 @@
 # Scrape pinnydb.com and take all their images for my own! Muhahahah!
+# Use scrape_individual for more detailed info
 
 from requests_html import HTMLSession
 #import urllib.request
 import csv
 import json
 
-
-pg = 14
+pg = 10
 base = 'https://pinnydb.com'
 url = f'{base}/?page={pg}'
 s = HTMLSession()
 r = s.get(url)
 
-with open('test.json', 'a', newline='') as f:
-#with open('info.txt', 'a', newline='') as f:
-    wr = csv.writer(f)
+outfile = open('test.json', 'w')
 
-    while (r.status_code == 200 and pg < 15):
+while (r.status_code == 200 and pg < 15):
 
-        r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=20)
-        boxes = r.html.find('.pinbox > a')
+    r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=20)
+    boxes = r.html.find('.pinbox > a')
 
-        for box in boxes:
-            # id
-            pin_id = box.attrs['href'].split('/')[-1]
+    for box in boxes:
+        # id
+        pin_id = box.attrs['href'].split('/')[-1]
 
-            # pin name
-            pname = box.find('.pinname', first=True).text
+        # pin name
+        pname = box.find('.pinname', first=True).text
 
-            # pin set (if there is one)
-            pset = box.find('.pinset', first=True).text
+        # pin set (if there is one)
+        pset = box.find('.pinset', first=True).text
 
-            # pin image url
-            purl = box.find('.thumb > img', first=True).attrs['src']
-            ext = purl.split('.')[-1]
-            #urllib.request.urlretrieve(f'{base}{purl}', f'{pin_id}.{ext}')
-            #print(f'{base}{purl} -- ', f'{pin_id}.{ext}')
-            #print(f'{pin_id}: {pname} ({base}{purl})')
-            #row = [pin_id, pname, pset, purl, f'{pin_id}.{ext}']
-            dictStr = {'id': pin_id, 'name': pname, 'set': pset, 'url': purl, 'filename': f'{pin_id}.{ext}'}
-            row = json.dumps(dictStr, indent=4)
-            wr.writerow(row)
+        # pin image url
+        purl = box.find('.thumb > img', first=True).attrs['src']
+        ext = purl.split('.')[-1]
 
-        pg = pg + 1
-        url = f'{base}/?page={pg}'
-        r = s.get(url)
-s.close()
+        #row = [pin_id, pname, pset, purl, f'{pin_id}.{ext}']
+        dictStr = {'id': pin_id, 'name': pname, 'set': pset, 'url': purl, 'filename': f'{pin_id}.{ext}'}
+        #print(dictStr)
+        row = json.dump(dictStr, outfile, indent=4)
+
+    pg = pg + 1
+    url = f'{base}/?page={pg}'
+    r = s.get(url)
+
+outfile.close()
