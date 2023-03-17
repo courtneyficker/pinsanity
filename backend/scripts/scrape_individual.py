4fc0ba416a30308af807fe509e4d799974e44395
Courtney Ficker <courtney.ficker@gmail.com>
2023-03-17 16:12:26 -0700

    Further changes to script. Can be refined further after term, working ok now

--- backend/scripts/scrape_individual.py
+++ backend/scripts/scrape_individual.py
@@ -3,18 +3,48 @@
 from requests_html import HTMLSession
 import json
 
-base = 'https://pinnydb.com/pinDetail/'
+base = 'https://pinnydb.com/'
 s = HTMLSession()
+filePath = '../src/lib/pinfo_test.json'
+
+fileCount = 0
+
+def checkWebLatest():
+    r = s.get(base)
+    r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=40)
+    boxes = r.html.find('.pinbox > a')
+    pin_id = boxes[0].attrs['href'].split('/')[-1]
+
+    print("Newest ID on website:", pin_id)
+    return int(pin_id)
+
+def checkFileLatest():
+    try:
+        global fileCount
+        with open(filePath, 'r') as f:
+            fileCount = json.load(f)["count"]
+        print("Last ID in file:", fileCount)
+        return fileCount
+    except FileNotFoundError:
+        print("File does not exist. Full update incoming.")
+        return 0
 
 def scrape(pid):
-    url = f'{base}{pid}'
+    success = False
+
+    url = f'{base}pinDetail/{pid}'
     r = s.get(url)
 
-    r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=20)
-    infobox = r.html.find('.detail-box', first=True)
-
     pinfo = {}
-    pinfo['id'] = pid
+    while (not success):
+        r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=40)
+        infobox = r.html.find('.detail-box', first=True)
+
+        pinfo['id'] = pid
+        if (pinfo['id'] == ""):
+            print("Failed...trying again")
+        else:
+            success = True
     pinfo['name'] = infobox.find('h3', first=True).text
     print(f'Looking up info on pin #{pid}...FOUND {pinfo["name"]}')
     pinfo['info'] = infobox.find('p', first=True).text
@@ -38,9 +68,61 @@ def scrape(pid):
             pinfo[key] = value
     return pinfo
 
+def checkUpdateNeeded():
+    print("Checking if update is required...")
+    local = checkFileLatest()
+    web = checkWebLatest()
 
-with open('pinfo.json', 'a') as f:
-    for id in range(1,1346):
-        json.dump(scrape(id), f, indent=4)
-        #f.write('\n')
+    if (web > local):
+        print("Update needed!")
+        return local, web
+    print("Currently up to date")
+    return 0,0
+
+def update(start,end):
+    # Build list of new pins (each a dict)
+    newPins = []
+    for id in range(start,end):
+        pin = scrape(id)    # dict
+        newPins.append(pin)
+    newCount = len(newPins)
+    
+    # Now grab our current list
+    try:
+        with open(filePath, 'r') as f:
+            previousPins = json.load(f)['pins']
+            oldCount = fileCount
+    except FileNotFoundError:
+        previousPins = []
+        oldCount = 0
+
+    # Now combine them
+    combinedTotal = oldCount + newCount
+    newPinsList = previousPins + newPins
+
+    print(f'Created a list of {oldCount}+{newCount}={combinedTotal} items.')
+    # print(newPinsList)
+
+    newFile = {
+        "count": combinedTotal,
+        "pins": newPinsList
+    }
+
+    #Update file
+    with open(filePath, 'w') as f:
+        json.dump(newFile, f, indent=4)
+
+if __name__ == "__main__":
+    start, end = checkUpdateNeeded()
+    if (end == 0):
+        print("Exiting")
+        exit()
+    
+    # Overwriting start/end for testing purposes
+    if (False):  ## Change to True for testing
+        start, end = 2, 4   # Grab specific pins (+1s applied in loop)
+    print(f'Updating pins from {start+1} to {end}')
+
+    update(start+1, end+1)
+    
 s.close()
